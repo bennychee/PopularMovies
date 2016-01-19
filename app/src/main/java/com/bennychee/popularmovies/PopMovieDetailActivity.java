820791c4580f374d417f1b0797aa59a538b11205
bennychee <chee.benny@gmail.com>
2016-01-19 16:41:04 +0800

    Movie Details Loader

--- app/src/main/java/com/bennychee/popularmovies/PopMovieDetailActivity.java
+++ app/src/main/java/com/bennychee/popularmovies/PopMovieDetailActivity.java
@@ -2,6 +2,7 @@ package com.bennychee.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class PopMovieDetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,27 @@ public class PopMovieDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_movie_detail);
+
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(PopMovieDetailActivityFragment.DETAIL_URI, getIntent().getData());
+
+            PopMovieDetailActivityFragment fragment = new PopMovieDetailActivityFragment();
+            fragment.setArguments(arguments);
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.pop_movie_detail_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+     //   getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
     }
 }
--- app/src/main/java/com/bennychee/popularmovies/PopMovieDetailActivityFragment.java
+++ app/src/main/java/com/bennychee/popularmovies/PopMovieDetailActivityFragment.java
@@ -1,28 +1,58 @@
 package com.bennychee.popularmovies;
 
-
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bennychee.popularmovies.api.MovieService;
+import com.bennychee.popularmovies.api.models.review.MovieReviews;
+import com.bennychee.popularmovies.api.models.runtime.MovieRuntime;
+import com.bennychee.popularmovies.api.models.trailers.MovieTrailers;
+import com.bennychee.popularmovies.api.models.review.Result;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PopMovieDetailActivityFragment extends Fragment {
+public class PopMovieDetailActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private static final String LOG_TAG = PopMovieDetailActivityFragment.class.getSimpleName();
+    private static final int DETAIL_LOADER = 0;
+    static final String DETAIL_URI = "URI";
+
+    private Uri mUri;
 
     public PopMovieDetailActivityFragment() {
         // Required empty public constructor
+        setHasOptionsMenu(true);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            mUri = arguments.getParcelable(PopMovieDetailActivityFragment.DETAIL_URI);
+        }
+
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_pop_movie_detail_activity, container, false);
         Intent intent = getActivity().getIntent();
@@ -30,9 +60,131 @@ public class PopMovieDetailActivityFragment extends Fragment {
             return null;
         }
 
-
-
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (null != mUri) {
+
+            int movieId = Utility.fetchMovieIdFromUri(getActivity(), mUri);
+            LoadMovieDetails(movieId);
+
+            return new CursorLoader(
+                    getActivity(),
+                    mUri,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+
+    private void LoadMovieDetails (final int movieId) {
+
+        String apiKey = BuildConfig.MOVIE_DB_API_TOKEN;
+        String baseUrl = BuildConfig.API_BASE_URL;
+
+        Log.d(LOG_TAG, "Base URL = " + baseUrl);
+        Log.d(LOG_TAG, "API Key = " + apiKey);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        final MovieService service = retrofit.create(MovieService.class);
+
+        MovieRuntime(movieId, apiKey, service);
+        MovieReview(movieId, apiKey, service);
+        MovieTrailers(movieId, apiKey, service);
+    }
+
+    private void MovieTrailers(final int movieId, String apiKey, MovieService service) {
+        Call<MovieTrailers> movieTrailersCall = service.getMovieTrailer(movieId, apiKey);
+        movieTrailersCall.enqueue(new Callback<MovieTrailers>() {
+            @Override
+            public void onResponse(Response<MovieTrailers> response) {
+                Log.d(LOG_TAG, "Movie Trailers Response Status: " + response.code());
+                if (!response.isSuccess()) {
+                    Log.e(LOG_TAG, "Unsuccessful Call for Trailer " + movieId + " Response: " + response.errorBody().toString());
+                } else {
+                    List<com.bennychee.popularmovies.api.models.trailers.Result> trailersResultList = response.body().getResults();
+                    Log.d(LOG_TAG, "Movie ID: " + movieId + " Trailers Added: " + trailersResultList.size());
+                    Utility.storeTrailerList(getContext(), movieId, trailersResultList);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(LOG_TAG, "Movie Trailer Error: " + t.getMessage());
+            }
+        });
+    }
+
+    private void MovieReview(final int movieId, String apiKey, MovieService service) {
+        Call<MovieReviews> movieReviewsCall = service.getMovieReview(movieId, apiKey);
+        movieReviewsCall.enqueue(new Callback<MovieReviews>() {
+            @Override
+            public void onResponse(Response<MovieReviews> response) {
+                Log.d(LOG_TAG, "Movie Reviews Response Status: " + response.code());
+                if (!response.isSuccess()) {
+                    Log.e(LOG_TAG, "Unsuccessful Call for Reviews " + movieId + " Response: " + response.errorBody().toString());
+                } else {
+                    List<Result> reviewResultList = response.body().getResults();
+                    Log.d(LOG_TAG, "Movie ID: " + movieId + " Reviews Added: " + reviewResultList.size());
+                    Utility.storeCommentList(getContext(), movieId, reviewResultList);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(LOG_TAG, "Movie Review Error: " + t.getMessage());
+            }
+        });
+    }
+
+    private void MovieRuntime(final int movieId, String apiKey, MovieService service) {
+        Call<MovieRuntime> movieRuntimeCall = service.getMovieRuntime(movieId, apiKey);
+        movieRuntimeCall.enqueue(new Callback<MovieRuntime>() {
+            @Override
+            public void onResponse(Response<MovieRuntime> response) {
+                Log.d(LOG_TAG, "Movie Runtime Response Status: " + response.code());
+                if (!response.isSuccess()) {
+                    Log.e(LOG_TAG, "Unsuccessful Call for Runtime " + movieId + " Response: " + response.errorBody().toString());
+                } else {
+                    int runtime = response.body().getRuntime();
+                    Log.d(LOG_TAG, "Movie ID: " + movieId + " Runtime: " + runtime);
+                    Utility.updateMovieWithRuntime(getContext(), movieId, runtime);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(LOG_TAG, "Movie Runtime Error: " + t.getMessage());
+            }
+        });
+    }
+
 }
+
